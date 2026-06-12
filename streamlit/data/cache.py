38588696b20025ed2bc7e618805a660cbe47c4f1
FDA PharmaVigilance Team <dev@pharmavigilance.local>
2026-06-11 20:18:21 -0600

    Cleanup: remove unused code and redundant docs

--- streamlit/data/cache.py
+++
@@ -1,9 +0,0 @@
-"""Caching utilities for Streamlit"""
-
-import streamlit as st
-from datetime import timedelta
-
-
-def cache_data(ttl_hours: int = 1):
-    """Cache data for specified hours"""
-    return st.cache_data(ttl=timedelta(hours=ttl_hours))
